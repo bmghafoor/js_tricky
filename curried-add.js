e8edd0bfd5515759e92a0868922dee47b1d44f56
Binyameen <bmghafoor@gmail.com>
2023-08-13 23:26:56 -0400

    add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,13 @@
 function curriedAdd(total) {
-
+  // if no inital input arg return 0
+  if (total === undefined) return 0;
+  return function addNext(num) {
+    // if no input arg just return the total
+    if (num === undefined) return total;
+    // if an input arg we add that to total and do recursion
+    total += num;
+    return addNext;
+  };
 }
 
 module.exports = { curriedAdd };
