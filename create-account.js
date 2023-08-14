f975ddc8756c942eba5b683bf77e4f96cf2c07b6
Binyameen <bmghafoor@gmail.com>
2023-08-13 23:06:07 -0400

    deposit

--- create-account.js
+++ create-account.js
@@ -7,6 +7,14 @@ function createAccount(pin, amount = 0) {
         return "Invalid PIN.";
       }
     },
+    deposit(code, amt) {
+      if (code === pin) {
+        amount += amt;
+        return `Succesfully deposited $${amt}. Current balance: $${amount}.`;
+      } else {
+        return "Invalid PIN.";
+      }
+    },
   };
 }
 
