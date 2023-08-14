5e1b9f352b8b019144cfe3421a698c692d7fcb89
Binyameen <bmghafoor@gmail.com>
2023-08-13 23:03:15 -0400

    check balance

--- create-account.js
+++ create-account.js
@@ -1,5 +1,13 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, amount = 0) {
+  return {
+    checkBalance(code) {
+      if (code === pin) {
+        return `$${amount}`;
+      } else {
+        return "Invalid PIN.";
+      }
+    },
+  };
 }
 
 module.exports = { createAccount };
