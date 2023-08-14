94a916029ec47081e085cae0e286fa5bd894ada9
Binyameen <bmghafoor@gmail.com>
2023-08-13 22:55:10 -0400

    guessing game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,21 @@
-function guessingGame() {
+function guessingGame(target) {
+  let count = 0;
+  let gameOver = false;
+  return function check(guess) {
+    if (gameOver === true) {
+      return "The game is over, you already won!";
+    }
+    count++;
 
+    if (guess > target) {
+      return `${guess} is too high!`;
+    } else if (guess < target) {
+      return `${guess} is too low!`;
+    } else {
+      gameOver = true;
+      return `You win! You found ${guess} in ${count} guesses.`;
+    }
+  };
 }
 
 module.exports = { guessingGame };
